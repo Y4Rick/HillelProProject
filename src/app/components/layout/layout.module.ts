60afb5f8944a64e50dc5e93f310472f2215548f2
YaR1ck <krasatuk@gmail.com>
2020-02-09 10:28:43 +0200

    finish layout

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { Overlay } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [LayoutComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
-  ]
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+  ],
+  providers : [Overlay]
 })
 export class LayoutModule { }
